ca2c8ce1a5465ee7e516a6527b8874989eef5866
Roman <dizels@gmail.com>
2020-06-13 18:04:34 +0300

    Внес изменения, чтобы все запускалось как модуль

--- spider/__main__.py
+++ spider/__main__.py
@@ -1,6 +1,6 @@
 from telethon import TelegramClient, sync
-from settings import API_ID, API_HASH
-from channel_info import get_channel_info
+from .settings import API_ID, API_HASH
+from .channel_info import get_channel_info
 
 # Подключаемся
 client = TelegramClient('socpulce_tg', API_ID, API_HASH)
