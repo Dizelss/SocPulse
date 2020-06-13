48844f0e4372cbe6a1392bf8b0d6997387d49bbb
Roman <dizels@gmail.com>
2020-06-13 13:20:26 +0300

    Создал отдельную ветку и сформировал тестовый запрос по АПИ - вывод кол-ва подписчиков паблика

--- spider/__init__.py
+++ spider/__init__.py
@@ -1,16 +1,13 @@
 from telethon import TelegramClient, sync
 from settings import API_ID, API_HASH
-
+from telethon import functions, types
 
 client = TelegramClient('socpulce_tg', API_ID, API_HASH)
 client.start()
 
-# выводим информацию о своем аккаунте
-# print(client.get_me().stringify())
-
-# отправляем сообщение другому аккаунту
-# client.send_message('romanN005', 'Привет - это сообщение отправлено с помощью библиотеки Telethon')
-
-# получаем информацию по каналу
-# channel = client.get_entity("https://t.me/pythonjuniorjob")
-# print(channel)
+result = client(functions.channels.GetFullChannelRequest(
+    channel='pythonjuniorjob'
+))
+# Выводим кол-во подписчиков паблика
+print(result.stringify())
+print(f"Количество подписчиков паблика: {result.full_chat.participants_count}")
