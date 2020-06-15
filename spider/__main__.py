592876508e63347e299be8fe0571d2e249770ecc
Roman <dizels@gmail.com>
2020-06-15 15:06:28 +0300

    Добавил файл с используемыми модулями и их версиями (requirements.txt) и почистил код

--- spider/__main__.py
+++ spider/__main__.py
@@ -2,15 +2,9 @@ from telethon import TelegramClient, sync
 
 from .settings import API_ID, API_HASH
 from .channel_info import update_channel_info
-from .model import db
 
 def start_spider():
-    # app.config.from_pyfile("settings.py")
-    # db.init_app(app)
-
     client = TelegramClient('socpulce_tg', API_ID, API_HASH)
     client.start()
 
     update_channel_info(client, "pythonjuniorjob")
-
-    return app
