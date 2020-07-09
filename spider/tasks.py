3cf250f46d12c0e393d93853b21d021180d17078
Roman <dizels@gmail.com>
2020-07-09 20:28:12 +0300

    корректировки ф-ции tg_name_to_analyze

--- spider/tasks.py
+++ spider/tasks.py
@@ -3,7 +3,7 @@ from .channel_info import get_channel_info, update_channel_info
 
 
 @app.task(name="tg_names_to_analyze")
-def tg_names_to_analyze(tg_name):
-    result_info_channel = get_channel_info(start[0], tg_name)
-    update_channel_info(result_info_channel, start[1])
+def tg_names_to_analyze(tg_name, start0, start1):
+    result_info_channel = get_channel_info(start0, tg_name)
+    update_channel_info(result_info_channel, start1)
     return f"задача выполнена, {result_info_channel}"
